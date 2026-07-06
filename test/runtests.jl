9bce0448e357861c2ddf2e14cfd6a8b2647ae44e
Ross Viljoen <ross.viljoen@graystonetrading.com>
2026-07-06 12:11:53 +0200

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,5 +9,6 @@ include("test_data.jl")
     include("ethernet_tests.jl")
     include("ip_tests.jl")
     include("udp_tests.jl")
+    include("igmp_tests.jl")
     include("dispatch_tests.jl")
 end
